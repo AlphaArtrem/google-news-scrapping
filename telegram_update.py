7f1475fee18a0d21d946b0032cf1499a349175ee
AlphaArtrem <yashawasthi01@gmail.com>
2020-11-11 01:29:49 +0530

    New bots

--- telegram_update.py
+++ telegram_update.py
@@ -16,8 +16,8 @@ creds = json.load(cred_file)
 cred_file.close()
 telegram_creds = creds["telegram"]
 
-def telegram_bot_sendtext(bot_message, messageID):
-    bot_token = telegram_creds["bot_id"]
+def telegram_bot_sendtext(bot_message, messageID, bot):
+    bot_token = telegram_creds[bot]
     bot_chatID = messageID
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
@@ -25,9 +25,9 @@ def telegram_bot_sendtext(bot_message, messageID):
 
     return response.json()
 
-def get_user_subs():
+def get_user_subs(csvName):
     user_subs = {}
-    with open(current_path + f"/csv/users.csv", 'r', encoding='windows-1252') as csvfile: 
+    with open(current_path + f"/csv/{csvName}.csv", 'r', encoding='windows-1252') as csvfile: 
             csvreader = csv.reader(csvfile) 
             # Ignore csv headers
             _ = next(csvreader)
@@ -45,9 +45,17 @@ def get_user_subs():
 
 # Search scrapper
 def telegram_updates(query, ts, keyword, trending = 0):
-    user_subs = get_user_subs()
-    for user in user_subs[query]:
-        if trending > 0:
-            telegram_bot_sendtext(f"keyword : {keyword} for entity : {query} has been trending for {trending} hours", user)
-        else:
-            telegram_bot_sendtext("entity : " + query + "\ntimestamp : " + ts + "\nkeyword : " +  keyword, user)
+    user_subs = get_user_subs("SuperUsers")
+    specific_users = get_user_subs("SpecificUsers")
+    if query in user_subs.keys():
+        for user in user_subs[query]:
+            if trending > 0:
+                telegram_bot_sendtext(f"keyword : {keyword} for entity : {query} has been trending for {trending} hours", user, "super")
+            else:
+                telegram_bot_sendtext("entity : " + query + "\ntimestamp : " + ts + "\nkeyword : " +  keyword, user, "super")
+    if query in specific_users.keys():
+        for user in specific_users[query]:
+            if trending > 0:
+                telegram_bot_sendtext(f"keyword : {keyword} for entity : {query} has been trending for {trending} hours", user, query)
+            else:
+                telegram_bot_sendtext("entity : " + query + "\ntimestamp : " + ts + "\nkeyword : " +  keyword, user, query)
